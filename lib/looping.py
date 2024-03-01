279039c7346fd0961b23d41ef75d01992ac59781
wachuka7 <rachaelwachuka7@gmail.com>
2024-03-01 16:34:12 +0300

    add contional functions

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i>=1:
+        print(i)
+        i -=1
+
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squares=[num**2 for num in int_list]
+    return squares
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num %3==0 and num % 5==0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+fizzbuzz()
+
